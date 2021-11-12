0bb2dcd10647014e29669ec563887fe7ef014965
Prabhdeep <prabhdeeprandhawa16369@gmail.com>
2021-11-12 11:12:04 +0530

    added readme and done with some changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -29,7 +29,7 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
     }
 
     req.body.images = imagesLinks
-    // req.body.user = req.user.id;
+    req.body.user = req.user.id;
 
     const product = await Product.create(req.body);
 
