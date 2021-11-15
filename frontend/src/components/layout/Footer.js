1cf0f72bc2d4e0e2aaee55b20f4e0ed09696f725
Prabhdeep <prabhdeeprandhawa16369@gmail.com>
2021-11-15 16:23:21 +0530

    done with product range & footer modification

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import "../../App.css";
 
 const Footer = () => {
-    return (
-        <Fragment>
-            <footer className="py-1">
-                <p className="text-center mt-1">
-                    Shopping Cart - 2019-2020, All Rights Reserved
-                </p>
-            </footer>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <footer id="footer">
+        <div className="leftFooter">
+          <h4>Welcome !</h4>
+          <p>--</p>
+        </div>
 
-export default Footer
+        <div className="midFooter">
+          <h1>Prabh-Ecommerce</h1>
+          <p>| High Quality Is Our First Priority |</p>
+
+          <p>Shopping Cart - 2020-2021, All Rights Reserved</p>
+        </div>
+
+        <div className="rightFooter">
+          <h4>Follow Us</h4>
+          <a href="http://instagram.com/">Instagram</a>
+        </div>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Footer;
